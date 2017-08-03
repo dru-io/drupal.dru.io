f8091937222a9d36a7f5099869314f1199e7ee01
Niklan <niklanrus@gmail.com>
2017-08-03 20:18:48 +0500

    Rewrited menu.js

--- themes/custom/classier/js/menu.js
+++ themes/custom/classier/js/menu.js
@@ -1,115 +1,129 @@
-(function ($, Drupal, drupalSettings) {
+/* eslint-disable no-console */
+/**
+ * @file
+ * Menu behaviors.
+ */
+
+(function ($, window, Drupal) {
 
   'use strict';
 
-    
-    //Animated menu
-    var height = $('header').offset().top;
-    $(window).scroll(function(){
-        if ($(document).height() > $(window).height()+$('header').height()) {
-            var scroll = $(this).scrollTop();
-            if (scroll >= 1) {
-                $('header').addClass('animated-menu');
-            }
-            else {
-                $('header').removeClass('animated-menu');
-            }
-        }
-    });
-    $(window).load(function(){
-        if ($(document).height() > $(window).height()+$('header').height()) {
-            var scroll = $(this).scrollTop();
-            if (scroll >= 1) {
-                $('header').addClass('animated-menu');
-            }
-            else {
-                $('header').removeClass('animated-menu');
-            }
-        }
-    });
-    //Мобильное меню
-    $('.region-upper-header .region-inner').append('<div id="csmenu"><span>button</span></div>');
-    $('#csmenu').click(function () {
-        $('body').toggleClass('menu--open');
-        $('.region-menu').toggleClass('menu--open');
-    });
-    $(document).click(function (e){ // событие клика по веб-документу
-        var div = $("#csmenu"); // тут указываем ID элемента
-        if (!div.is(e.target) // если клик был не по нашему блоку
-            && div.has(e.target).length === 0) { // и не по его дочерним элементам
-            $('.region-menu').removeClass('menu--open'); // скрываем его
-            $('body').removeClass('menu--open'); // скрываем его
-        }
-    });
+  /**
+   * Fix menu during scroll to the top of the page and deattach when is scrolled
+   * to the upper position.
+   */
+  Drupal.behaviors.promo_menu_fixating = {
+    attach: function (context, settings) {
 
-    var menuheight = $('header').height() - 15; // Высота меню на desktop.
-    /* Плавный скролл */
-    $('#block-classier-main-menu .menu-item a').click(function () { 
-        var elementClick = $(this).attr("href");
-        var destination = $(elementClick).offset().top - menuheight;
-        $("html, body").animate( { scrollTop: destination }, 500);
-        $('body').removeClass('menu--open');
-        $('.region-menu').removeClass('menu--open');
-        return false;
-    });
-    $('.field--name-field-button a[href^="#"]').click(function () { 
-        var elementClick = $(this).attr("href");
-        var destination = $(elementClick).offset().top - menuheight;
-        $("html, body").animate( { scrollTop: destination }, 500);
-        return false;
-    });
-    $('a[href^="#"]').click(function () { 
-        var elementClick = $(this).attr("href");
-        var destination = $(elementClick).offset().top - menuheight;
-        $("html, body").animate( { scrollTop: destination }, 500);
-        return false;
-    });
-
-
-    /* КОНЕЦ */
-    
-    $(window).load(function () {
-        //Добавляем значение высоты блоков по каждому пункту меню
-        $('#block-classier-main-menu .menu-item').each(function () {
-            var ids = $('a', this).attr('href');
-            var height = $(ids).offset().top - 70;
-            $(this).attr('height', height);
-        });        
-        menu($(this).scrollTop());
-    });      
-    $(window).scroll(function () {
-        menu($(this).scrollTop());
-    });
-        
-    function menu(scroll) {
-        //Присваиваем класс активному пункту меню
-        for (var i = 0; i <= $('#block-classier-main-menu .menu-item').length; i++) {
-            var height_1 = $('#block-classier-main-menu .menu-item').eq(i).attr('height');
-            var height_2 = $('#block-classier-main-menu .menu-item').eq(i+1).attr('height');
-            if (height_2 !== undefined) {
-                if (scroll >= height_1 && scroll < height_2) { //Не первый и не последний
-                    $('#block-classier-main-menu .menu-item').removeClass('current');
-                    $('.animated-menu #block-classier-main-menu .menu-item').removeClass('current');
-                    $('#block-classier-main-menu .menu-item').eq(i).addClass('current');
-                    $('.animated-menu #block-classier-main-menu .menu-item').eq(i).addClass('current');
-                    break;
-                } else if (scroll < height_1) { //Первый пункт
-                    $('#block-classier-main-menu .menu-item').removeClass('current');
-                    $('.animated-menu #block-classier-main-menu .menu-item').removeClass('current');
-                    $('#block-classier-main-menu .menu-item:first').addClass('current');
-                    $('.animated-menu #block-classier-main-menu .menu-item:first').addClass('current');
-                    break;                    
-                }              
-            } else {
-                if (scroll >= height_1) { //Последний
-                    $('#block-classier-main-menu .menu-item').removeClass('current');
-                    $('.animated-menu #block-classier-main-menu .menu-item').removeClass('current');
-                    $('#block-classier-main-menu .menu-item:last').addClass('current');
-                    $('.animated-menu #block-classier-main-menu .menu-item:last').addClass('current');
-                    break;
-                }                
-            }
+      /**
+       * Toggle class according to current scroll position.
+       */
+      const toggleMenuFixation = function () {
+        const scrollPosition = $(window).scrollTop();
+        if (scrollPosition) {
+          $('header').addClass('animated-menu');
         }
-    }     
-      
-})(jQuery, Drupal, drupalSettings);
+        else {
+          $('header').removeClass('animated-menu');
+        }
+      };
+
+      // Call it first time during page load, because page can be loaded not
+      // at the top of window.
+      toggleMenuFixation();
+
+      // Bind on scroll.
+      $(window).on('scroll', function () {
+        toggleMenuFixation();
+      });
+
+    }
+  };
+
+  /**
+   * Mobile menu.
+   */
+  Drupal.behaviors.promo_menu_mobile = {
+    attach: function (context, settings) {
+
+      // Create and add markup for mobile menu button to the header.
+      let $mobileButtonHtml = $('<div id="csmenu"><span>button</span></div>');
+      $('.region-upper-header .region-inner', context).append($mobileButtonHtml);
+
+      // Add event listened for this button.
+      $mobileButtonHtml.once('mobile-button').on('click', function () {
+        $('body, .region-menu').toggleClass('menu--open');
+      });
+
+      // Add click listener for closing menu on parent elements click.
+      $(document).once('mobile-scroll').on('click', function (event) {
+        if (!$mobileButtonHtml.is(event.target) && $mobileButtonHtml.has(event.target).length === 0) {
+          $('body, .region-menu').removeClass('menu--open');
+        }
+      });
+
+    }
+  };
+
+  /**
+   * Smooth scrolling page to needed anchor.
+   */
+  Drupal.behaviors.promo_menu_smooth_scroll = {
+    attach: function (context, settings) {
+
+      const menuHeight = $('header').height();
+      // Smooth scroll.
+      const smoothScroll = function (element) {
+        const destinationElement = $(element).attr('href');
+        const destinationPosition = $(destinationElement).offset().top - menuHeight;
+        $('body').animate({scrollTop: destinationPosition}, 500);
+      };
+
+      $('#block-classier-main-menu .menu-item a,' +
+        '.field--name-field-button a[href^="#"],' +
+        'a[href^="#"]').once('smooth-click').on('click', function (event) {
+        smoothScroll(event.target);
+      });
+
+    }
+  };
+
+  /**
+   * Smooth scrolling page to needed anchor.
+   *
+   * @todo maybe do it better.
+   */
+  Drupal.behaviors.promo_menu_active_item = {
+    attach: function (context, settings) {
+
+      // Storage for all available anchors in menu.
+      let anchorList = [];
+      const menuItemSelector = '#block-classier-main-menu .menu-item';
+      // Function to set class for active menu item.
+      const setActiveMenuItem = function () {
+        const windowScroll = $(window).scrollTop();
+        $(anchorList).each(function (i) {
+          if ($(this).offset().top <= windowScroll + 150) {
+            if ($(menuItemSelector).hasClass('current')) {
+              $(menuItemSelector).removeClass('current');
+            }
+            $(menuItemSelector).eq(i).addClass('current');
+          }
+        });
+      };
+
+      $(menuItemSelector).each(function (i, element) {
+        anchorList.push($('a', element).attr('href'));
+      });
+
+      $(window).once('active-menu-item').on('scroll', function () {
+        setActiveMenuItem();
+      });
+
+      // On page load.
+      setActiveMenuItem();
+
+    }
+  };
+
+})(jQuery, window, Drupal);
